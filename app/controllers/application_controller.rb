be1d01b0d02f0d7db80fe4f0c93d2df5387c5c99
Sugawara Kei <kei_s8221@icloud.com>
2020-09-24 15:16:40 +0900

    hello,world

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+    def hello
+        render html: "hello,world!!"
+    end
+
+    def goodbye
+        render html: "goodbye,world!!"
+    end
+end
